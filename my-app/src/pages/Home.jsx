ac932c167420560f74dc4bb9a1413c17fbd11f05
nayan457-bit <nayan.krishna.s68@kalvium.community>
2025-02-19 12:18:29 +0530

    update

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
     return (
         <div className="container">
-            <h1>Irine here!</h1>
-            <p>Mail me: irine.email@example.com</p>
+            <h1>Nayan here!</h1>
+            <p>Mail me: nayan.email@example.com</p>
             <p>Call me: +00000000</p>
         </div>
     );
